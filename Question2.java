8f702224f278ec585d444f20324d1918ce758f4f
Evremk <80709842+Evremk@users.noreply.github.com>
2021-03-21 22:53:20 +0100

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,10 @@
+package day2assignment;
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Turing"+", "+"Alan");
+	}
+
+}
---
+++ Question8.java
@@ -0,0 +1,19 @@
+package day2assignment;
+
+public class Question8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("**        **    ***     ****");
+System.out.println("***      ***    ***    ***");
+System.out.println("*****   ****    ***   **");
+System.out.println("***  *** ***    *** **");
+System.out.println("***   *  ***    ****** ");
+System.out.println("***      ***    ***   **");
+System.out.println("***      ***    ***    ***");
+System.out.println("***      ***    ***     ***");
+System.out.println("***      ***    ***      ****");
+
+	}
+
+}
